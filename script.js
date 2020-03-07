702d4bb64e6c31de99fc583f302e991f544d3879
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-03-07 21:24:20 +1030

    Variables created for the password criteria.

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ else {
     var password = "";
     var specialCaseCharacters = confirm("Would you like to use Special Characters?");
     if (specialCaseCharacters === true) {
-      choiceArray.push
+      choiceArray.push("@", "#", "$", "%", "^", "&", "*", "(", ")", "!", "-", "_", "+", "?", ",", ".", "[", "]", "{", "}", "~", ":", ";", "<", ">", "/");
     }
     var numericalCaseCharacters = confirm("Would you like to use Numbers?");
     if (numericalCaseCharacters === true){
-      choiceArray.push
+      choiceArray.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
     }
     var lowerCaseCharacters = confirm("Would you like to use Lower case Characters?");
     if (lowerCaseCharacters === true){
-      choiceArray.push
+      choiceArray.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
     }
     var upperCaseCharacters = confirm("Would you like to use Upper case Characters?");
     if (upperCaseCharacters === true){
-      choiceArray.push
+      choiceArray.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "j", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
     }
 }
