3744f5705b9c1c90b755f6764214bf6550795ebe
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-03-07 21:16:14 +1030

    Prompts and conditions added for the password criteria.

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function getRandomPassword(){
+  var choiceArray = [];
+  var passwordPrompt = prompt("How many characters would you like your new password to be? Pick a number between 8 and 128.");
+  var getLength = parseInt(passwordPrompt);
+  if (getLength < 8 || getLength > 128) {
+      alert("Please choose a number between 8 and 128.")
+  }
+else {
+    var password = "";
+    var specialCaseCharacters = confirm("Would you like to use Special Characters?");
+    if (specialCaseCharacters === true) {
+      choiceArray.push
+    }
+    var numericalCaseCharacters = confirm("Would you like to use Numbers?");
+    if (numericalCaseCharacters === true){
+      choiceArray.push
+    }
+    var lowerCaseCharacters = confirm("Would you like to use Lower case Characters?");
+    if (lowerCaseCharacters === true){
+      choiceArray.push
+    }
+    var upperCaseCharacters = confirm("Would you like to use Upper case Characters?");
+    if (upperCaseCharacters === true){
+      choiceArray.push
+    }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
